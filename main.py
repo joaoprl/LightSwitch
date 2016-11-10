43e780994cac476964afe8c6def46a69bc20bf69
jpedrorl <jpedrorl@gmail.com>
2016-11-09 23:41:21 -0200

    Update main.py to receive the server port via command-lineUp

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import clap
 import sys
 
 def main():
+    if(len(sys.argv) == 2):
+        port = sys.argv[1]
+    else:        
+        port = 20000
+
     relay = usbrelay.Relay()
     relay.open()
-
+    
     server = androidServer.Server(20000, relay)
     server.run()
 
     clapListener = clap.ClapListener(relay)
-    clapListener.run()
+#    clapListener.run()
 
     string = ""
     while string != "exit":
